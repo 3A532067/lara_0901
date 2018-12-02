fb966ea4eb22dd808e90ad01dfd8bd4a079ba10b
3A532067 <s3a532067@student.ncut.edu.tw>
2018-12-02 12:37:47 +0800

    debug

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -24,11 +24,9 @@
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
 
-            @foreach($errors->all()as $error)
+            @foreach ($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
-
-
         </div>
     </div>
 </div>
